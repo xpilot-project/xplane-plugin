f51878cb096064eb3c745d8487297ef887ad2c17
Justin Shannon <zla.jshannon@gmail.com>
2020-08-02 19:55:56 -0700

    Update version

--- Constants.h
+++ Constants.h
@@ -25,7 +25,7 @@ constexpr int COLOR_GREEN = 0x00FF00;
 constexpr int COLOR_BLUE = 0x00F0F0;
 
 #define PLUGIN_NAME "xPilot"
-#define PLUGIN_VERSION 1321
-#define PLUGIN_VERSION_STRING "1.3.21"
+#define PLUGIN_VERSION 1322
+#define PLUGIN_VERSION_STRING "1.3.22"
 
 #endif // !Constants_h
