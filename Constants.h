546d3e9bf5ac6672afe54c1339a1ae4fd46ff57e
Justin Shannon <zla.jshannon@gmail.com>
2020-08-12 20:36:43 -0700

    Update plugin version

--- Constants.h
+++ Constants.h
@@ -25,7 +25,7 @@ constexpr int COLOR_GREEN = 0x00FF00;
 constexpr int COLOR_BLUE = 0x00F0F0;
 
 #define PLUGIN_NAME "xPilot"
-#define PLUGIN_VERSION 1327
-#define PLUGIN_VERSION_STRING "1.3.27+002"
+#define PLUGIN_VERSION 1328
+#define PLUGIN_VERSION_STRING "1.3.28"
 
 #endif // !Constants_h
