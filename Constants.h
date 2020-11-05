de09c5bd65f9cb1dc8c59f5d1691a2dd9e797334
Justin Shannon <zla.jshannon@gmail.com>
2020-11-04 21:39:32 -0800

    Implement instancing validation logging

--- Constants.h
+++ Constants.h
@@ -25,7 +25,7 @@ constexpr int COLOR_GREEN = 0x00FF00;
 constexpr int COLOR_BLUE = 0x00F0F0;
 
 #define PLUGIN_NAME "xPilot"
-#define PLUGIN_VERSION 1332
-#define PLUGIN_VERSION_STRING "1.3.32"
+#define PLUGIN_VERSION 1333
+#define PLUGIN_VERSION_STRING "1.3.33"
 
 #endif // !Constants_h
