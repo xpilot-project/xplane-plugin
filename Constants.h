3deef9e4a1486a1e380dba4acf68b39f98dd86c2
Justin Shannon <zla.jshannon@gmail.com>
2021-01-27 20:40:10 -0800

    Update XPMP2 lib to include nullptr check for XPLMInstanceRef's

--- Constants.h
+++ Constants.h
@@ -25,7 +25,7 @@ constexpr int COLOR_GREEN = 0x00FF00;
 constexpr int COLOR_BLUE = 0x00F0F0;
 
 #define PLUGIN_NAME "xPilot"
-#define PLUGIN_VERSION 1336
-#define PLUGIN_VERSION_STRING "1.3.36"
+#define PLUGIN_VERSION 1337
+#define PLUGIN_VERSION_STRING "1.3.37"
 
 #endif // !Constants_h
